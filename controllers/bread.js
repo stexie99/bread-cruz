f69958f190fdc2a398227bddc3742a83d735e4d4
Steven Xie <xies1999@hotmail.com>
2023-07-14 12:26:19 -0400

    added index view

--- controllers/bread.js
+++ controllers/bread.js
@@ -1,8 +1,10 @@
 const router = require('express').Router()
 const Bread = require('../models/bread')
+//Get all bread
 router.get('/',(req,res)=>{
-    res.send(Bread)
+    res.render('index', { breads: Bread })
 })
+//get by index
 router.get('/:index',(req,res)=>{
     const{ index }= req.params
     res.send(Bread[index])
--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express')
 require('dotenv').config()
 const breadController = require('./controllers/bread')
+
 const app = express()
+
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
 app.use('/breads', breadController)
+
 const PORT = process.env.PORT
+
 app.listen(PORT, console.log(`listening on port ${PORT}`))
