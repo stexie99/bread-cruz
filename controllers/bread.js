73fd9e4c9132b1404aa72c6da0a4398f76b6bce8
Steven Xie <xies1999@hotmail.com>
2023-07-15 01:20:30 -0400

    added ability to create new bread

--- controllers/bread.js
+++ controllers/bread.js
@@ -4,9 +4,23 @@ const Bread = require('../models/bread')
 router.get('/',(req,res)=>{
     res.render('index', { breads: Bread })
 })
+router.get('/new', (req,res)=>{
+    res.render('new')
+})
 //get by index
 router.get('/:index',(req,res)=>{
     const{ index }= req.params
     res.render('show', {bread: Bread[index]})
+})
+
+router.post('/', (req, res)=>{
+    if (req.body.hasGluten === 'on'){
+        req.body.hasGluten = true
+    } else{
+        req.body.hasGluten = false
+    }
+    if(!req.body.image) req.body.image='https://images.unsplash.com/photo-1534620808146-d33bb39128b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80'
+    Bread.push(req.body)
+    res.redirect('/breads')
 })
 module.exports = router
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const breadController = require('./controllers/bread')
 
 const app = express()
 
+app.use(express.urlencoded({ extended: true}))
 app.use(express.static('public'))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
